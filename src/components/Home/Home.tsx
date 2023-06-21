23500926ef630c7cedd3c93da4e200db50ceefa3
Hyun-Tae Jin <htjmario@gmail.com>
2023-06-20 23:25:16 -0400

    updated description

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { NavBar } from "../shared/NavBar";
 
 interface Props {
@@ -14,15 +15,25 @@ export const Home = (props: Props) => {
             How it works: I take your collection off your hands and will sell
             your rare/mythic/expensive cards for you.
           </div>
-          <div className="my-4">We only get paid if you get paid!</div>
+          <div className="my-4 animate-trance">We only get paid if you get paid!</div>
           <div>
             There is a X% fee charged for the service. Please note, any bulk
             commons or uncommons will be sold at 5¢ per uncommon and 3¢ per
             common.
           </div>
           <div className="mt-4">
-            Please feel free to schedule a pick up, drop off or consultation. If
-            you already have an account, click here to log in
+            Please feel free to schedule a pick up, drop off or consultation.
+          </div>
+          <div>
+            Already have an account?
+            <div className="mt-10 mx-auto">
+              <Link
+                className="font-xl animate-tranceBg rounded-full px-5 py-3 hover:outline hover:outline-[var(--hover-color)]"
+                to={"/login"}
+              >
+                Log Me In!
+              </Link>
+            </div>
           </div>
           {/* -- I think this should be behind some kind of fee details
           button to show exact rates. I would need the rates to be customizable.
